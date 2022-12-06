017cfd6a4ce87349fc69b36203c3c5e87993ecdc
web-altaf-1 <web.altaf.1@gmail.com>
2022-12-07 00:31:23 +0600

    admin route added

---
+++ src/pages/AdminPanel/AdminPanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AdminPanel = () => {
+
+    return (
+        <div>
+            <h2 className='text-center py-2'>Admin Panel</h2>
+            <p>comming soon</p>
+        </div>
+    );
+};
+
+export default AdminPanel;
